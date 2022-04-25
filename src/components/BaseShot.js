46f112f19ed6947a04e888a032cf1a4ede739cd4
superdeveloper718 <103200988+topdevmaster@users.noreply.github.com>
2022-04-25 11:38:03 -0800

    9

--- src/components/BaseShot.js
+++ src/components/BaseShot.js
@@ -21,7 +21,7 @@ let backAudio = loadSound('bMusic', true)
 backAudio.loop = true;
 backAudio.volume = 0.15;
 
-let currentSceneNumber = 7;
+let currentSceneNumber = 8;
 
 let bodyAudio1 = loadSound(audioPrifix + '01')
 let bodyAudio2 = loadSound(audioPrifix + '02')
